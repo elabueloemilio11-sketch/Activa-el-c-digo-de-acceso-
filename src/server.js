e4532ea23e6ad1d7be134c99e907e0f77ac1db91
elabueloemilio11-sketch <elabueloemilio11@gmail.com>
2026-09-11 01:26:14 +0200

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import { buildApp } from './app.js';
 import { loadConfig } from './config.js';
+import { directAccessRoutes } from './routes/direct-access.js';
 
 const config = loadConfig();
 const app = await buildApp({ config });
+await directAccessRoutes(app);
 
 const shutdown = async (signal) => {
   app.log.info({ signal }, 'Graceful shutdown started');
